1ff4c24b65964356abba554304b9566cf42ff66e
Txwxcc <drutk@softserveinc.com>
2017-08-08 13:36:07 +0200

    assumptions

--- src/app.js
+++ src/app.js
@@ -1,17 +1,69 @@
-import source from './features/modules/source';
-import handler from './handler';
-import './css/style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import source from './features/modules/source';
+// import handler from './handler';
+// import './css/style.scss';
+// import Select from './features/modules/select/components/select';
 /**
  * Bootstrapping application
  */
 function bootstrap() {
     console.log('~ Bootstrapping App');
 
-    console.log('~ Init select');
-    source.bootstrap();
+    // console.log('~ Init select');
+    // source.bootstrap();
+    //
+    // console.log('~ Init drop-down menu');
+    // handler.dropDown();
 
-    console.log('~ Init drop-down menu');
-    handler.dropDown();
+    function Source(props) {
+        return <hr />;
+    }
+
+    function InputFancyField(props) {
+        return <input />;
+    }
+
+    function InputDataList(props) {
+        return <ul />;
+    }
+
+    function Input(props) {
+        return (
+            <div>
+                <InputFancyField />
+                <InputDataList />
+            </div>
+        );
+    }
+
+    function Selects(props) {
+        return (
+            <div>
+                {['s1', 's2', 's3'].map(s => <Input name={s} />)}
+            </div>
+        );
+    }
+
+    function Results(props) {
+        return <hr />;
+    }
+
+    function App(props) {
+        return (
+            <div>
+                <Source />
+                <Selects />
+                <Results />
+            </div>
+        );
+    }
+
+    const appDock = document.createElement('div');
+    document.body.appendChild(appDock);
+
+    ReactDOM.render(<App />, appDock);
 }
 
 bootstrap();
